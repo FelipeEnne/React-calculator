2b927c30be576ee146d143ade57b6e87ee6563d8
FelipeEnne <felipeenne@gmail.com>
2020-04-24 17:30:37 -0300

    add the big.js

---
+++ README.md
@@ -0,0 +1,1 @@
+A calculator using create-react-app
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Big from './big.mjs';
 import './index.css'
 
 const root = document.getElementById('root');
