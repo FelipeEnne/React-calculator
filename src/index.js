88a6b2a038f265194ca87fde257a6ee35f6aefea
FelipeEnne <felipeenne@gmail.com>
2020-04-27 14:39:27 -0300

    add linter files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDom from 'react-dom';
 import Big from 'big.js';
 import PropTypes from 'prop-types';
-import './index.css'
+import './index.css';
 
 const root = document.getElementById('root');
